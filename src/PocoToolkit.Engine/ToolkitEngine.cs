9cc8ed789813fcb5294514cf42aaf0d462fdc01c
mqlabib <mqlabib@gmail.com>
2026-08-05 06:01:46 +0700

    feat(engine): introduce ToolkitEngine facade

---
+++ src/PocoToolkit.Engine/ToolkitEngine.cs
@@ -0,0 +1,22 @@
+using PocoToolkit.Contracts.Abstractions;
+
+namespace PocoToolkit.Engine;
+
+public sealed class ToolkitEngine
+{
+    private readonly IAdbService _adb;
+
+    public ToolkitEngine(IAdbService adb)
+    {
+        _adb = adb;
+    }
+
+    public Task<bool> IsAdbInstalledAsync()
+        => _adb.IsInstalledAsync();
+
+    public Task<string?> GetAdbVersionAsync()
+        => _adb.GetVersionAsync();
+
+    public string? GetAdbPath()
+        => _adb.GetPath();
+}
